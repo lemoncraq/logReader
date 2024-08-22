a5a8a07adfc95c93cb8ca23181a2f47335909b68
makov <boobasucker@gmail.com>
2024-08-22 22:46:28 +0700

    feat:
    -[x] Чтение файла в потоке;
    -[x] Получение входных данных (проценты, милисекунды);
    -[x] Класс записи лога;
    -[x] Парсинг строк;
    -[x] Анализ отказов;
    -[x] Расчет доступности;
    -[x] Вывод результатов;
    -[x] Сборка артефакта.
    
    TODO:
    -[ ] Проверки/условия;
    -[ ] Тесты.

--- src/main/java/LogAnalyzer.java
+++ src/main/java/LogAnalyzer.java
@@ -1,26 +1,82 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LogAnalyzer {
+    private static final String logPattern =
+            //IP
+            "^(\\S+)" +
+                    " - - " +
+                    //DateTime
+                    "\\[(\\d{2}/\\d{2}/\\d{4}:\\d{2}:\\d{2}:\\d{2} [+-]\\d{4})]" +
+                    " \".*\" " +
+                    //Response code
+                    "(\\d{3})" +
+                    " \\d+ " +
+                    //Response time
+                    "(\\d+\\.\\d+)";
+    private static final Pattern pattern = Pattern.compile(logPattern);
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss");
+
     public static void main(String[] args) throws Exception {
         double minAvailability = Double.parseDouble(args[0]);
         double maxResponseTime = Long.parseLong(args[1]);
-        //TODO: Для сборки переделать на InputStreamReader(System.in)
-        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/access.log"));
 
-        List<LogEntry> logEntries = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Date startInterval = null;
+        int totalRequests = 0;
+        int failedRequests = 0;
 
         String line;
-
         while ((line = reader.readLine()) != null) {
-//            LogEntry entry
-            System.out.println(line);
+            LogEntry logEntry = parseLogEntry(line);
+            if (logEntry != null) {
+                totalRequests++;
+                boolean isFailure = logEntry.getResponseCode() >= 500 || logEntry.getResponseTime() >= maxResponseTime;
+                if (isFailure) {
+                    failedRequests++;
+                }
+
+                double availability = 100.0 * (totalRequests - failedRequests) / totalRequests;
+
+                if (availability < minAvailability) {
+                    if (startInterval == null) {
+                        startInterval = logEntry.getDateTime();
+                    } else {
+                        System.out.println("Interval: " +
+                                sdf.format(startInterval) +
+                                " - " +
+                                sdf.format(logEntry.getDateTime()) +
+                                " Availability: " +
+                                availability + "%");
+
+                        startInterval = null;
+                        totalRequests = 0;
+                        failedRequests = 0;
+                    }
+                }
+            }
         }
-        System.out.println(minAvailability);
-        System.out.println(maxResponseTime);
+    }
+
+    private static LogEntry parseLogEntry(String line) {
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()) {
+            try {
+                Date dateTime = sdf.parse(matcher.group(2));
+                int responseCode = Integer.parseInt(matcher.group(3));
+                long responseTime = (long) Double.parseDouble(matcher.group(4));
+
+                return new LogEntry(dateTime, responseCode, responseTime);
+            } catch (ParseException | NumberFormatException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
     }
 }
